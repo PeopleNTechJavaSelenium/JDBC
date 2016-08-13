1b013df9729177e2e6c12276df52b2f6f7cdc0f9
mrahman <rahmanww@gmail.com>
2016-08-13 14:41:58 -0400

    read db

--- src/jdbc/TestConnection.java
+++ src/jdbc/TestConnection.java
@@ -2,11 +2,18 @@ package jdbc;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestConnection {
-	public static void main(String [] args) throws ClassNotFoundException, IOException, SQLException{
+	public static void main(String [] args) throws Exception{
 		ConnectDB connectDB = new ConnectDB();
-		connectDB.connectToDatabase();
+		List<String> list = new ArrayList<String>();
+		list = connectDB.readDataBase("cnnnewsvertical", "NewsTitle");
+		System.out.println("Data is reading from the Table and displaying to the console");
+		for(String st:list){
+			System.out.println(st);
+		}
 	}
 
 }
