54f9ef11611613274e8971f5fa9d939424a6c4ac
mrahman <rahmanww@gmail.com>
2016-08-14 12:02:28 -0400

    read data

--- src/mongodb/ConnectMongoDB.java
+++ src/mongodb/ConnectMongoDB.java
@@ -1,8 +1,14 @@
 package mongodb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -15,10 +21,28 @@ public class ConnectMongoDB {
 		System.out.println("mongodb is connected");
 		return mdb;
 	}
-	public void insert(String collectionName,String newsType, String writerName){
+	public MongoDatabase connectMongoRemote(String databaseName){
+		MongoClient mClient = new MongoClient();
+		mdb = mClient.getDatabase(databaseName);
+		System.out.println("mongodb is connected");
+		return mdb;
+	}
+	public void insert(String databaseName,String collectionName,String newsType, String writerName, String date){
+		mdb = connectMongoRemote(databaseName);
 		MongoCollection mCollection = mdb.getCollection(collectionName);
-		Document doc = new Document().append("news",newsType).append("writer", writerName);
+		Document doc = new Document().append("news",newsType).append("writer", writerName).append("date", date);
 		mCollection.insertOne(doc);
+	}
+	public List<Document> getMongoDBData(String databaseName, String collectionName){
+		List<Document> list = new ArrayList<Document>();
+		mdb = connectMongo(databaseName);
+		MongoCollection coll = mdb.getCollection(collectionName);	
+		List<Document> docList = ((List<Document>)coll.find().into(new ArrayList<Document>()));
+		for(Document doc:docList){
+			list.add(doc);
+		}
+		
+		return list;
 	}
 
 }
--- src/mongodb/TestMongoDB.java
+++ src/mongodb/TestMongoDB.java
@@ -1,11 +1,23 @@
 package mongodb;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+
+import com.mongodb.util.JSON;
+
 public class TestMongoDB {
 
 	public static void main(String[] args) {
 		ConnectMongoDB mongo = new ConnectMongoDB();
-		mongo.connectMongo("test");
-		mongo.insert("NewsContent", "Tech", "Steve Job");
+		mongo.insert("test", "NewsContent", "USA","","08/14/2016");
+		List<Document> list = new ArrayList<Document>();
+		list = mongo.getMongoDBData("test", "NewsContent");
+		for(Document doc:list){
+			System.out.println(JSON.parse(doc.toJson()));
+		}
+		
 
 	}
 
